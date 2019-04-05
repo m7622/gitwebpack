9f8e5caaafac94aad4b7c826e7f75eb8b57a7162
m7622 <ster.yhes@gmail.com>
2019-04-05 19:36:51 +0800

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './assets/js/script.js',
     output: {
-        filename: 'bundle.js',
+        filename: './dist/bundle.js',
         path: path.resolve(__dirname, './'),
     }
 };

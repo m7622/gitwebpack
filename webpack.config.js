6cf6564040544f58411e2fb72a6c28252a5905cc
m7622 <ster.yhes@gmail.com>
2019-04-05 19:25:17 +0800

    add assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 
 const path = require('path');
 module.exports = {
-    entry: './index.js',
+    entry: './assets/js/script.js',
     output: {
-        filename: 'index.bundle.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, './'),
     }
 };

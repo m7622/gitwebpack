0d8f3a814df6b26a255967e36dd02156deb05259
m7622 <ster.yhes@gmail.com>
2019-04-06 12:41:06 +0800

    js

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
     output: {
         filename: './final/assets/js/index.js',
         path: path.resolve(__dirname, './'),
-    }
+    },
+    module:{
+        rules:[{
+            test:/\.(js)$/,
+            exclude:/(node_modules)/,
+            use:{
+                loader:'babel-loader',
+                options:{
+                    presets:['@babel/preset-env']
+                }
+            }
+        }]
+    }    
+            
+            
 };

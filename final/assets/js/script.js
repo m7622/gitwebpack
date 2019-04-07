bc75f041befef7f7ce730a65f1bab623d8cf0a38
m7622 <ster.yhes@gmail.com>
2019-04-07 15:32:41 +0800

    postcss

--- final/assets/js/script.js
+++ final/assets/js/script.js
@@ -1,8 +1,27 @@
-import b from '../images/bird.jpg';
-import s from '../images/squirrel.jpg';
+import"../css/fonts/PT-Sans-Bold.eot";
+import"../css/fonts/PT-Sans-Bold.svg";
+import"../css/fonts/PT-Sans-Bold.ttf";
+import"../css/fonts/PT-Sans-Bold.woff";
+import"../css/fonts/PT-Sans-BoldItalic.eot";
+import"../css/fonts/PT-Sans-BoldItalic.svg";
+import"../css/fonts/PT-Sans-BoldItalic.ttf";
+import"../css/fonts/PT-Sans-BoldItalic.woff";
+import"../css/fonts/PT-Sans-Italic.eot";
+import"../css/fonts/PT-Sans-Italic.svg";
+import"../css/fonts/PT-Sans-Italic.ttf";
+import"../css/fonts/PT-Sans-Italic.woff";
+import"../css/fonts/PT-Sans.eot";
+import"../css/fonts/PT-Sans.svg";
+import"../css/fonts/PT-Sans.ttf";
+import"../css/fonts/PT-Sans.woff";
+import $ from 'jquery';
+import '../css/bootstrap.min.css';
+import '../css/style.css';
+import bi from '../images/bird.jpg';
+import sq from '../images/squirrel.jpg';
 
-$('#bird').attr("src",b);
-$('#squirrel').attr("src",s);
+$('#bird').attr("src",bi);
+$('#squirrel').attr("src",sq);
 
 $(document).ready(function () {
 	// when a hanburger menu is clicked
